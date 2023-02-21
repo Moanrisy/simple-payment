361f99b78dd62cd6a8b6d65d9ca635a1a40e98f3
Moanrisy <moanrisy@gmail.com>
2023-02-21 20:42:59 +0700

    feat: add swagger for bank endpoints

--- docs/docs.go
+++ docs/docs.go
@@ -15,79 +15,6 @@ const docTemplate = `{
     "host": "{{.Host}}",
     "basePath": "{{.BasePath}}",
     "paths": {
-        "/api/banks": {
-            "get": {
-                "tags": [
-                    "bank"
-                ],
-                "summary": "Get all banks",
-                "responses": {
-                    "200": {
-                        "description": "OK",
-                        "schema": {
-                            "$ref": "#/definitions/model.Bank"
-                        }
-                    }
-                }
-            },
-            "post": {
-                "tags": [
-                    "bank"
-                ],
-                "summary": "Create new bank",
-                "responses": {
-                    "200": {
-                        "description": "OK"
-                    }
-                }
-            }
-        },
-        "/api/banks/id": {
-            "put": {
-                "tags": [
-                    "bank"
-                ],
-                "summary": "Topup bank by ID",
-                "responses": {
-                    "200": {
-                        "description": "OK",
-                        "schema": {
-                            "$ref": "#/definitions/model.Bank"
-                        }
-                    }
-                }
-            }
-        },
-        "/api/banks/{id}": {
-            "get": {
-                "tags": [
-                    "bank"
-                ],
-                "summary": "Get bank by ID",
-                "responses": {
-                    "200": {
-                        "description": "OK",
-                        "schema": {
-                            "$ref": "#/definitions/model.Bank"
-                        }
-                    }
-                }
-            },
-            "delete": {
-                "tags": [
-                    "bank"
-                ],
-                "summary": "Delete bank by ID",
-                "responses": {
-                    "200": {
-                        "description": "OK",
-                        "schema": {
-                            "$ref": "#/definitions/model.Bank"
-                        }
-                    }
-                }
-            }
-        },
         "/api/customers": {
             "get": {
                 "tags": [
@@ -226,10 +153,7 @@ const docTemplate = `{
                 ],
                 "responses": {
                     "200": {
-                        "description": "OK",
-                        "schema": {
-                            "$ref": "#/definitions/model.Customer"
-                        }
+                        "description": "OK"
                     }
                 }
             }
@@ -333,73 +257,13 @@ const docTemplate = `{
                 ],
                 "responses": {
                     "200": {
-                        "description": "OK",
-                        "schema": {
-                            "$ref": "#/definitions/model.Merchant"
-                        }
-                    }
-                }
-            }
-        },
-        "/api/payments": {
-            "get": {
-                "tags": [
-                    "payment"
-                ],
-                "summary": "Get all payments",
-                "responses": {
-                    "200": {
-                        "description": "OK",
-                        "schema": {
-                            "$ref": "#/definitions/model.Payment"
-                        }
-                    }
-                }
-            },
-            "post": {
-                "tags": [
-                    "payment"
-                ],
-                "summary": "Create new payment",
-                "responses": {
-                    "200": {
                         "description": "OK"
-                    }
-                }
-            }
-        },
-        "/api/payments/{id}": {
-            "get": {
-                "tags": [
-                    "payment"
-                ],
-                "summary": "Get payment by ID",
-                "responses": {
-                    "200": {
-                        "description": "OK",
-                        "schema": {
-                            "$ref": "#/definitions/model.Payment"
-                        }
                     }
                 }
             }
         }
     },
     "definitions": {
-        "model.Bank": {
-            "type": "object",
-            "properties": {
-                "balance": {
-                    "type": "string"
-                },
-                "bank_account_number": {
-                    "type": "string"
-                },
-                "bank_id": {
-                    "type": "string"
-                }
-            }
-        },
         "model.Customer": {
             "type": "object",
             "properties": {
@@ -480,29 +344,6 @@ const docTemplate = `{
             "properties": {
                 "data": {},
                 "message": {
-                    "type": "string"
-                }
-            }
-        },
-        "model.Payment": {
-            "type": "object",
-            "properties": {
-                "amount": {
-                    "type": "string"
-                },
-                "bank_account_number": {
-                    "type": "string"
-                },
-                "created_at": {
-                    "type": "string"
-                },
-                "payment_id": {
-                    "type": "string"
-                },
-                "receiver_id": {
-                    "type": "string"
-                },
-                "sender_id": {
                     "type": "string"
                 }
             }
